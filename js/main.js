0844e946c8623b8d42b6ac9bf76d7ac790e9930b
rizqysap <149395559+rizqysap@users.noreply.github.com>
2023-10-31 09:51:32 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+
+
+	$("#portfolio-contant-active").mixItUp();
+
+	$(function() {
+		$('a.page-scroll').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+
+
+	$("#testimonial-slider").owlCarousel({
+	    paginationSpeed : 500,      
+	    singleItem:true,
+	    autoPlay: 3000,
+	});
+
+
+
+
+	$("#clients-logo").owlCarousel({
+		autoPlay: 3000,
+		items : 5,
+		itemsDesktop : [1199,5],
+		itemsDesktopSmall : [979,5],
+	});
+
+	$("#works-logo").owlCarousel({
+		autoPlay: 3000,
+		items : 5,
+		itemsDesktop : [1199,5],
+		itemsDesktopSmall : [979,5],
+	});
+
+
+	// google map
+		var map;
+		function initMap() {
+		  map = new google.maps.Map(document.getElementById('map'), {
+		    center: {lat: -34.397, lng: 150.644},
+		    zoom: 8
+		  });
+		}
+
+
+	// Counter
+
+	$('.counter').counterUp({
+        delay: 10,
+        time: 4500
+    });
+
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top'
+    });
+
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+});
+
+
+
+
